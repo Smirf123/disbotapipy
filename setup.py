7e008bfa04e8f30ecfbeadd9f6a713c0c563d56f
Smirf123 <smirf368@outlook.com>
2020-12-03 09:47:54 -0600

    inital commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Disbotapipy", # Replace with your own username
+    version="1.0.0",
+    author="Smirf123",
+    author_email="matthewelert@gmail.com",
+    description="A simple api wrapper for the Disbot.top API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/smirf123/disbotapipy",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)

ea57fe92ffe8019eb6ef0e47d0183afa00e1e8e5
Smirf123 <smirf368@outlook.com>
2021-06-08 22:27:12 -0700

    Update to V2

--- disbotapipy/disbotapipy.py
+++ disbotapipy/disbotapipy.py
@@ -3,7 +3,7 @@ import requests
 class disbot():
 
     def updateStats(self, clientid, token, serverCount:int):
-        sendURL = f'https://disbot.top/api/v1/botupdate/{clientid}'
+        sendURL = f'https://disbot.top/api/v2/botupdate/{clientid}'
         body = {'serverCount': serverCount}
         header = {'authorization': token}
         success = requests.post(sendURL, data = body, headers=header)
